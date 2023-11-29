2acbfd15021e3531e4b97998150d6ee15afe806e
Joao Victor Alonso de Paula Sperandio <joaovictorapsperandio@gmail.com>
2023-11-29 11:31:46 -0300

    game

---
+++ src/scripts/engine.js
@@ -0,0 +1,70 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll('.square'),
+        enemy: document.querySelector('.enemy'),
+        timeLeft: document.querySelector('#time-left'),
+        score: document.querySelector('#score'),
+    },
+    values:{
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions:{
+        moveEnemy: setInterval(randomSquare, 500),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function playAudio(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0){
+        clearInterval(state.values.countDownTimerId);
+        clearInterval(state.values.moveEnemy);
+        alert('GAME OVER! Seu resultado foi: ' + state.values.result);
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove('enemy');
+
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add('enemy');
+
+    state.values.hitPosition = randomSquare.id;
+
+}
+
+
+function addListenerHitBox(){
+    state.view.squares.forEach(square => {
+        square.addEventListener("mousedown", () => {
+            if(square.id == state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playAudio("hit");
+            }
+        });
+    });
+}
+
+function init(){
+    addListenerHitBox();
+    countDown();
+}
+
+init(); // start the game
